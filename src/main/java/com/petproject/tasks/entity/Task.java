395ef899049f6bf07588ec3bf6d06982eb7e12c6
EvgenyKuchko <kuchkozhenia@gmail.com>
2025-02-10 11:37:57 +0100

    Added DB connection and relationship between entities

--- src/main/java/com/petproject/tasks/entity/Task.java
+++ src/main/java/com/petproject/tasks/entity/Task.java
@@ -1,15 +1,23 @@
 package com.petproject.tasks.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@Entity( name = "tasks")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
     private LocalDate creationDate;
     private LocalDate dueDate;
+    @Enumerated(EnumType.STRING)
     private TaskStatus status;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
--- src/main/java/com/petproject/tasks/entity/User.java
+++ src/main/java/com/petproject/tasks/entity/User.java
@@ -1,11 +1,19 @@
 package com.petproject.tasks.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
+@Entity(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String login;
     private String password;
     private String name;
+    @OneToMany(mappedBy = "user")
+    private Set<Task> tasks;
 }
