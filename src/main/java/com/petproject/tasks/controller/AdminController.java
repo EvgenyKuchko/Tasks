599493e37193582f96178cd9c2a768c7eb53cbe7
EvgenyKuchko <kuchkozhenia@gmail.com>
2025-02-25 10:38:46 +0100

    removed date from new task form

--- src/main/java/com/petproject/tasks/controller/AdminController.java
+++ src/main/java/com/petproject/tasks/controller/AdminController.java
@@ -2,11 +2,13 @@ package com.petproject.tasks.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping("/admin")
+    @GetMapping
     public String showAdminPage() {
         return "admin";
     }
