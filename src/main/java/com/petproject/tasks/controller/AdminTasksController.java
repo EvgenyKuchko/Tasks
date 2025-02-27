ba2eeb174ddf1ef0543a7fb37780f2ddeb0a87a4
EvgenyKuchko <kuchkozhenia@gmail.com>
2025-02-27 16:54:29 +0100

    added function for searching tasks by keyword by admin in tasks.html

--- src/main/java/com/petproject/tasks/controller/AdminTasksController.java
+++ src/main/java/com/petproject/tasks/controller/AdminTasksController.java
@@ -7,8 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/admin")
@@ -18,8 +21,21 @@ public class AdminTasksController {
     private TaskService taskService;
 
     @GetMapping("/tasks")
-    public String showAdminTasksPage(Model model) {
+    public String showAdminTasksPage(Model model, @RequestParam(value = "keyword", required = false) String keyword,
+                                     @RequestParam(value = "userId", required = false) Long userId,
+                                     @RequestParam(value = "date", required = false) LocalDate date) {
         List<TaskDto> tasks = taskService.findAllTasks();
+
+        if (keyword != null && !keyword.isEmpty()) {
+            String lowerKeyword = keyword.toLowerCase();
+            tasks = tasks.stream()
+                    .filter(taskDto -> taskDto.getTitle().toLowerCase().contains(lowerKeyword) ||
+                            taskDto.getDescription().toLowerCase().contains(lowerKeyword))
+                    .collect(Collectors.toList());
+        }
+
+        TaskDto taskDto = new TaskDto();
+        model.addAttribute("task", taskDto);
         model.addAttribute("tasks", tasks);
         return "tasks";
     }
