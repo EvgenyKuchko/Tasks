e89972071978c83f877b41f58a860625c7861b9a
EvgenyKuchko <kuchkozhenia@gmail.com>
2025-03-26 16:03:09 +0100

    added validation by groups

--- src/main/java/com/petproject/tasks/dto/UserDto.java
+++ src/main/java/com/petproject/tasks/dto/UserDto.java
@@ -2,7 +2,6 @@ package com.petproject.tasks.dto;
 
 import com.petproject.tasks.entity.UserRole;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -14,16 +13,23 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-public class UserDto implements Dto{
+public class UserDto implements Dto {
     private Long id;
-    @NotBlank(message = "username cannot be empty")
-    @Size(min = 5, message = "username must be 5 or more characters")
+
+    @NotBlank(message = "username cannot be empty", groups = {OnCreate.class, OnUpdate.class})
+    @Size(min = 5, message = "username must be 5 or more characters", groups = {OnCreate.class, OnUpdate.class})
     private String username;
-    @NotBlank(message = "password cannot be empty")
-    @Size(min = 5, message = "password must be 5 or more characters")
+
+    @NotBlank(message = "password cannot be empty", groups = OnCreate.class) // Только при регистрации
+    @Size(min = 5, message = "password must be 5 or more characters", groups = {OnCreate.class, OnUpdate.class})
     private String password;
-    @NotBlank(message = "first name cannot be empty")
-    @Size(min = 2, message = "first name must be 2 or more characters")
+
+    @NotBlank(message = "first name cannot be empty", groups = {OnCreate.class, OnUpdate.class})
+    @Size(min = 2, message = "first name must be 2 or more characters", groups = {OnCreate.class, OnUpdate.class})
     private String firstName;
+
     private Set<UserRole> roles;
+
+    public interface OnCreate {}  // Валидация при создании
+    public interface OnUpdate {}  // Валидация при обновлении
 }
